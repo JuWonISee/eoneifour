c8bbfc28353d4133d64a3f2696fc79c3414f90f9
JuWonISee <tlswndnjs93@naver.com>
2025-06-20 17:24:12 +0900

    feat 전체입고율 구현

--- wms/src/main/java/com/eoneifour/wms/admin/view/AdminPage.java
+++ wms/src/main/java/com/eoneifour/wms/admin/view/AdminPage.java
@@ -7,10 +7,10 @@ import javax.swing.JPanel;
 
 import com.eoneifour.wms.home.view.MainFrame;
 
-public class AdminPage extends JPanel{
-	
+public class AdminPage extends JPanel {
+
 	private JFrame mainFrame;
-	
+
 	public AdminPage(MainFrame frame) {
 		this.mainFrame = frame;
 		setBackground(Color.BLACK);
