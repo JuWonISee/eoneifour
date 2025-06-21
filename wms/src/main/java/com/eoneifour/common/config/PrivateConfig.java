ea11d54d96efdbc040ff835648884bb850fcd27c
rekindle <102033760+rekindle402@users.noreply.github.com>
2025-06-21 20:53:17 +0900

    파일삭제 -PrivateConfig
    이그노어 되지 않은 PrivateConfig 삭제

--- wms/src/main/java/com/eoneifour/common/config/PrivateConfig.java
+++
@@ -1,7 +0,0 @@
-package com.eoneifour.common.config;
-
-public class PrivateConfig {
-	public static final String URL   = "jdbc:mysql://private:4406/test";
-	public static final String USER = "private";
-	public static final String PASS  = "private";
-}
