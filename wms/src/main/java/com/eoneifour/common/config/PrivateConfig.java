3fb6a54ef947bbef9944ee31ca9ccea804f6e1b0
jihoon-devstu <wlsgksvheh1@naver.com>
2025-06-23 14:45:52 +0900

    private config 삭제
    private config 삭제

--- wms/src/main/java/com/eoneifour/common/config/PrivateConfig.java
+++
@@ -1,7 +0,0 @@
-package com.eoneifour.common.config;
-
-public class PrivateConfig {
-	public static final String URL  = "jdbc:mysql://182.221.77.73:4406/test";
-    public static final String USER = "incuser";
-    public static final String PASS = "ssginc1234";
-}
